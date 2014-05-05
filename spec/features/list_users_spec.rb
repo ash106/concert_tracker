94c053ec710d499af2bae2e3b8257bc7c118c486
Alex Howington <alex.howington@gmail.com>
2014-05-04 22:54:46 -0600

    Add basic authorization to User actions

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -7,6 +7,8 @@ describe "Viewing the list of users" do
     user2 = User.create!(user_attributes(name: "Joe", email: "joe@hotmail.com"))
     user3 = User.create!(user_attributes(name: "Megan", email: "megan@gmail.com"))
 
+    sign_in(user1)
+
     visit users_url
 
     expect(page).to have_link(user1.name)
